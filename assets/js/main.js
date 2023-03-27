0a8fa429206fd44f5f55dac104be5e77988082fa
miportfoliowebb <129117514+miportfoliowebb@users.noreply.github.com>
2023-03-27 18:03:57 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/*=============== NAV ===============*/
+hamburger = document.querySelector(".hamburger");
+        nav = document.querySelector("nav");
+        hamburger.onclick = function() {
+            nav.classList.toggle("active");
+        }
+/*=============== contacto ===============*/
